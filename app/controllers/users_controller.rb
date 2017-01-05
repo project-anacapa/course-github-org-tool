b6bf1ca4e6a5da62434bf9669908a9dcb7a517e3
Nick Brown <nickbrown714@gmail.com>
2017-01-05 15:40:42 -0800

    restrict user listing to instructors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_instructor!, :except => [:match_to_student]
   before_action :correct_user?, :except => [:index, :set_as_instructor]
 
   def index
