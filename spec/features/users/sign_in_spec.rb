8bd48f178acac5584f2bd2287211f3815257bbb5
Nick Brown <nickbrown714@gmail.com>
2017-04-17 14:50:12 -0700

    un-commenting out the authentication specs

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -20,12 +20,11 @@ feature 'Sign in', :omniauth do
   #   When I sign in
   #   Then I see an authentication error message
   scenario 'user cannot sign in with invalid account' do
-    # OmniAuth.config.mock_auth[:twitter] = :invalid_credentials
-    # visit root_path
-    # expect(page).to have_content("Sign in")
-    # click_link "Sign in"
-    # expect(page).to have_content('Authentication error')
-    expect true
+    OmniAuth.config.mock_auth[:github] = :invalid_credentials
+    visit root_path
+    expect(page).to have_content("Sign in")
+    click_link "Sign in"
+    expect(page).to have_content('Authentication error')
   end
 
 end
--- spec/features/users/sign_out_spec.rb
+++ spec/features/users/sign_out_spec.rb
@@ -9,10 +9,9 @@ feature 'Sign out', :omniauth do
   #   When I sign out
   #   Then I see a signed out message
   scenario 'user signs out successfully' do
-    # signin
-    # click_link 'Sign out'
-    # expect(page).to have_content 'Signed out'
-    expect true
+    signin
+    click_link 'Sign out'
+    expect(page).to have_content 'Signed out'
   end
 
 end
