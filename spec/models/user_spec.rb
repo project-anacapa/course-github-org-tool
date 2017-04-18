ec4c903f6787b1a6659a04f5b3d49f0a7f91c0e6
Nick Brown <nickbrown714@gmail.com>
2017-04-17 20:44:17 -0700

    adding user and student model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,25 @@
+
 describe User do
 
-  before(:each) { @user = FactoryGirl.create(:user) }
+  before(:each) {
+    @user = FactoryGirl.create(
+        :user,
+         name: 'Test User',
+         username: 'test_user'
+    )
+  }
 
   subject { @user }
 
   it { should respond_to(:name) }
+  it { should respond_to(:username) }
 
-  it '#name returns a string' do
+  it "#name returns a string" do
     expect(@user.name).to match 'Test User'
+  end
+
+  it "#username returns a string" do
+    expect(@user.username).to match 'test_user'
   end
 
 end
