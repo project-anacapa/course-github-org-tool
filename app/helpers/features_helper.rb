42f46bed108543990465bfa49bf04b1aa3bb834a
Nick Brown <nickbrown714@gmail.com>
2017-05-01 08:24:05 -0700

    adding feature flag for anacapa repo things

---
+++ app/helpers/features_helper.rb
@@ -0,0 +1,18 @@
+module FeaturesHelper
+
+  def self.feature_enabled?(feature)
+    case feature
+      when 'features.anacapa_repos'
+        FeaturesHelper.anacapa_repos?
+      else
+        Setting[feature]
+    end
+  end
+
+  def self.anacapa_repos?
+    Setting['features.anacapa_repos']
+  end
+  def anacapa_repos?
+    FeaturesHelper.anacapa_repos?
+  end
+end
