332cc15b2b0b090a78ecb178df28d7d5be6d8da4
Connor Mulcahey <connormulcahey@me.com>
2017-05-17 13:51:00 -0700

    fixed bug to get csv upload working

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,18 +14,20 @@ class Student < ApplicationRecord
     full_name_index = header_map.index("full_name")
 
     # start at row 1 if header row exists (via checkbox)
-    ((header_row_exists ? 1 : 0 )..spreadsheet.last_row).each do |r|
+    ((header_row_exists ? 2 : 1 )..spreadsheet.last_row).each do |i|
 
-      row = {}
+      spreadsheet_row = spreadsheet.row(i)
 
-      row["student_id"] = r[id_index]
-      row["email"] = r[email_index]
+      row = {} # build dynaimically based on choices
+
+      row["student_id"] = spreadsheet_row[id_index]
+      row["email"] = spreadsheet_row[email_index]
 
       if first_name_index
-        row["first_name"] = r[first_name_index]
-        row["last_name"] = r[last_name_index]
+        row["first_name"] = spreadsheet_row[first_name_index]
+        row["last_name"] = spreadsheet_row[last_name_index]
       else
-        name_arr = r[full_name_index].split(" ") # this seems prone to bugs because last names are weird
+        name_arr = spreadsheet_row[full_name_index].split(" ") # this seems prone to bugs because last names are weird
         row["first_name"] = name_arr[0]
         row["last_name"] = name_arr[1]
       end
