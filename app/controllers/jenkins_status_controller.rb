eaf070dd19ff7a649314e1d156b536e2135a9c1b
Nick Brown <nickbrown714@gmail.com>
2017-06-03 14:56:59 -0700

    adding code to handle the jenkins job finalize notification

--- app/controllers/jenkins_status_controller.rb
+++ app/controllers/jenkins_status_controller.rb
@@ -2,7 +2,7 @@ class JenkinsStatusController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:index]
 
   def index
-    puts request
+    GenerateGradeJob.perform_later(params.as_json)
     head(:ok)
   end
 end
---
+++ app/jobs/generate_grade_job.rb
@@ -0,0 +1,29 @@
+class GenerateGradeJob < ApplicationJob
+  queue_as :default
+
+  def perform(payload)
+    name = payload['name'] # along the lines of `AnacapaGrader <domain> <org> (grader|assignment)-<lab>`
+    status = payload['build']['status']
+
+    match = /^AnacapaGrader (?<domain>\S+) (?<org>\S+) (?<type>grader|assignment)-(?<lab>\S+)$/i =~ name
+
+    logger.warn(payload)
+    logger.warn("Domain: #{domain}")
+    logger.warn("Org: #{org}")
+    logger.warn("Type: #{type}")
+    logger.warn("Lab: #{lab}")
+
+    if match != 0 # or domain != ENV['GIT_PROVIDER_URL'] or org != ENV['COURSE_ORGANIZATION']
+      return
+    end
+
+    if type == 'grader'
+      student = payload['build']['parameters']['github_user']
+      logger.warn("Grading assignment for #{student}")
+    elsif type == 'assignment'
+      # do nothing?
+    else # not identified? not possible?
+      # ignored
+    end
+  end
+end
